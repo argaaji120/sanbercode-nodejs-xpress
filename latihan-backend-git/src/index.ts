7cd5aa58c5056e072f202f3498295b5eb957010f
argaaji120 <argaaji120@gmail.com>
2024-11-14 16:52:12 +0700

    [Tugas Git] Add user authentication endpoints

---
+++ latihan-backend-git/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/register", (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    // code ...
+
+    res.status(200).json({ message: "User registered", data: null });
+});
+
+app.post("/login", (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    // code ...
+
+    res.status(200).json({ message: "User logged in", data: null });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
