c558ec8a5f5cb4cd2a5a213b90fe045bb0bf1980
argaaji120 <argaaji120@gmail.com>
2024-11-14 17:14:42 +0700

    [Tugas Git] Fix bugs for release 1.0.0

--- latihan-backend-git/src/index.ts
+++ latihan-backend-git/src/index.ts
@@ -7,33 +7,24 @@ app.use(express.json());
 
 app.post("/register", (req: Request, res: Response) => {
     const { username, password } = req.body;
-
     // code ...
-
     res.status(200).json({ message: "User registered", data: null });
 });
 
 app.post("/login", (req: Request, res: Response) => {
     const { username, password } = req.body;
-
     // code ...
-
     res.status(200).json({ message: "User logged in", data: null });
 });
 
 app.post("/products", (req: Request, res: Response) => {
     const { name, price } = req.body;
-
     // code ...
-
     res.status(200).json({ message: "Product added", data: null });
 });
 
 app.get("/products", (req: Request, res: Response) => {
-    const { name, price } = req.body;
-
     // code ...
-
     res.status(200).json({ message: "List of product", data: [] });
 });
 
